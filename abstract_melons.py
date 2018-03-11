5cfc24186c9d8769019e4d0e8d87143056d4797e
Hackbright Student <no-reply@hackbrightacademy.com>
2018-03-10 17:29:18 -0800

    Adds special price, increased shipping costs and special government prices :)

--- abstract_melons.py
+++ abstract_melons.py
@@ -16,6 +16,10 @@ class AbstractMelonOrder(object):
         """Calculate price, including tax."""
 
         base_price = 5
+
+        if self.species == 'Christmas melons':
+            base_price = base_price * 1.5
+
         total = (1 + self.tax) * self.qty * base_price
 
         return total
@@ -40,15 +44,35 @@ class InternationalMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty, country_code):
         super(InternationalMelonOrder, self).__init__(species, qty, country_code, 0.17, "international")
 
+    def get_total(self):
+        total = super(InternationalMelonOrder, self).get_total()
+
+        if self.qty < 10:
+            return total + 3
+        else:
+            return total
 
     def get_country_code(self):
         """Return the country code."""
 
         return self.country_code
 
+class GovernmentMelonOrder(AbstractMelonOrder):
+    def __init__(self, species, qty, country_code):
+        super(GovernmentMelonOrder, self).__init__(species, qty, country_code, 0, "government")
+        self.passed_inspection = False
+
+    def marked_inspection(self, passed=True):
+        self.passed_inspection = passed
+
 
 
 dom_melons = DomesticMelonOrder('cremshaw', 5)
 int_melons = InternationalMelonOrder('watermelon', 10, "GB")
+less_int_melon = InternationalMelonOrder('Christmas melons', 9, "GB")
+gov_melon = GovernmentMelonOrder('Yellow', 10, "GB")
+
 print dom_melons
 print int_melons
+print less_int_melon
+print gov_melon
